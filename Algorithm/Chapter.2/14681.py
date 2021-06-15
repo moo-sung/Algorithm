35a757f53ed94c62e520fe5a81d06dd42da4f1dd
moo-sung <58097202+moo-sung@users.noreply.github.com>
2021-06-15 15:15:07 +0900

    Day-2

--- Algorithm/Chapter.2/14681.py
+++ Algorithm/Chapter.2/14681.py
@@ -2,4 +2,17 @@
 # 사분면 고르기
 # Day-2
 
+import sys
 
+x = int(input())
+y = int(input())
+
+if x > 0 and y > 0:
+    a = 1
+elif x < 0 and y > 0:
+    a = 2
+elif x < 0 and y < 0:
+    a = 3
+else:
+    a = 4
+print(a)
