24f82ca0715766c6d91863d0d788ce6cc25838e0
moo-sung <58097202+moo-sung@users.noreply.github.com>
2021-06-28 12:28:18 +0900

    day-3

--- Algorithm/Chapter.3/10871.py
+++ Algorithm/Chapter.3/10871.py
@@ -2,3 +2,12 @@
 # X보다 작은 수
 # Day-3
 
+import sys
+
+n, x = map(int, sys.stdin.readline().split())
+input = sys.stdin.readline().split()
+input = list(map(int, input))
+
+for i in range(n):
+    if input[i] < x:
+        print(input[i], end=' ')
