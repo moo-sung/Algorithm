e6e4bf0432852b9241376fc32ffd74cf9f3ee5f6
moo-sung <58097202+moo-sung@users.noreply.github.com>
2021-06-28 12:12:26 +0900

    day-3

---
+++ Algorithm/Chapter.3/10871.py
@@ -0,0 +1,4 @@
+# https://www.acmicpc.net/problem/10871
+# X보다 작은 수
+# Day-3
+
--- Algorithm/Chapter.3/2439.py
+++ Algorithm/Chapter.3/2439.py
@@ -2,3 +2,12 @@
 # 별 찍기 - 2
 # Day-3
 
+import sys
+
+n = int(sys.stdin.readline())
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ", end='')
+    for j in range(i+1):
+        print("*", end='')
+    print()
