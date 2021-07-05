3c0195aa40802891b70a86b8e9ef99165d77b635
moo-sung <58097202+moo-sung@users.noreply.github.com>
2021-07-05 09:22:49 +0900

    .

---
+++ JavaAlgorithm/No_2309.java
@@ -0,0 +1,13 @@
+package JavaAlgorithm;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class No_2309 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        
+    
+        input.close();
+    }
+}
--- JavaAlgorithm/test.java
+++
@@ -1,7 +0,0 @@
-package JavaAlgorithm;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
