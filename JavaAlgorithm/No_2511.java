a3e0186c4c15e7a47cb0a86240f63128d7c2e35e
moo-sung <58097202+moo-sung@users.noreply.github.com>
2021-07-14 10:01:06 +0900

    Samsung

--- JavaAlgorithm/No_2511.java
+++ JavaAlgorithm/No_2511.java
@@ -1,5 +1,21 @@
 package JavaAlgorithm;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class No_2511 {
-    
+    public static void main(String[] args) throws Exception {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int[] scoreA = new int[10];
+        int[] scoreB = new int[10];
+        
+        StringTokenizer s = new StringTokenizer(input.readLine(), " ");
+
+
+        for (int i = 0; i < 10; i++) {
+            scoreA[i] = Integer.parseInt(s.nextToken());
+        }
+
+    }
 }
---
+++ SamsungSWExpertAcademy/UserSolution.java
@@ -0,0 +1,13 @@
+package SamsungSWExpertAcademy;
+
+class UserSolution {
+    public final static int N = 4;
+
+    public void doUserImplementation(int guess[]) {
+        // Implement a user's implementation function
+        //
+        // The array of guess[] is a return array that
+        // is your guess for what digits[] would be.
+    }
+}
+
